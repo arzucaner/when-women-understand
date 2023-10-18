94efc662825629faf4df347fb5c37b5fe180785d
Rose Le <rosehle@protonmail.com>
2023-10-18 16:24:57 -0700

    Basic Bootstrap styling to HomePage
    Co-authored-by: Rachel Ludwig <rae.ludwig42@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import LandingPage from "./pages/CommunityPage";
 import HomePage from "./pages/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
