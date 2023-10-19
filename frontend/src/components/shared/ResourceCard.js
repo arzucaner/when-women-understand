8c165db0c6f36879b547dd06af703ced0b6643e9
Raeludwig <racheleludwig@gmail.com>
2023-10-19 00:08:48 -0600

    Changed homepage and resourcecard

--- frontend/src/components/shared/ResourceCard.js
+++ frontend/src/components/shared/ResourceCard.js
@@ -1,5 +1,17 @@
-const ResourceCard = () => {
-  return <div>ResourceCard</div>;
+import React from 'react';
+import './ResourceCard.css';
+
+const ResourceCard = ({ image, title, description, link, bg }) => {
+  const cardClass = bg ? 'card-with-bg' : 'card-without-bg';
+
+  return (
+    <div className={`resource-card ${cardClass}`}>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <a href={link} className="learn-more-link">Learn More</a>
+    </div>
+  );
 };
 
 export default ResourceCard;
