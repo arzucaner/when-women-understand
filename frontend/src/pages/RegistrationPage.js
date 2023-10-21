04bd1522fa1b6d1d12ab4f7d8f08ca8658c211eb
Rose Le <rosehle@protonmail.com>
2023-10-21 00:53:47 -0700

    Fix layout for page

--- frontend/src/pages/RegistrationPage.js
+++ frontend/src/pages/RegistrationPage.js
@@ -3,47 +3,47 @@ import "../styles/Registration.css";
 
 const RegistrationPage = () => {
   return (
-    <div className="container-fluid registration-page">
-      <div className="row">
-        <div
-          className="col d-flex flex-column justify-content-between"
+    <div
+      className="container-fluid registration-page d-flex"
+      style={{ paddingLeft: 0, paddingRight: 0 }}
+    >
+      <div
+        className="col d-flex flex-column justify-content-between"
+        style={{
+          backgroundImage: `linear-gradient(rgba(31, 59, 33, 0.5), rgba(31, 59, 33, 0.5)), url('/Registration.jpg')`,
+          objectFit: "scale-down",
+          backgroundSize: "cover",
+        }}
+      >
+        <img
+          src="/Workmark white.png"
+          alt="When Women Understand Logo"
           style={{
-            backgroundImage: `linear-gradient(rgba(31, 59, 33, 0.5), rgba(31, 59, 33, 0.5)), url('/Registration.jpg')`,
-            objectFit: "scale-down",
-            aspectRatio: "1/1.5",
-            backgroundSize: "cover",
+            height: "auto",
+            width: "auto",
+            maxWidth: "10rem",
+            maxHeight: "auto",
+            paddingLeft: "2.5rem",
+            paddingTop: "1.8rem",
+          }}
+        ></img>
+        <div
+          className="align-self-end justify-self-start text-start text-light"
+          style={{
+            fontFamily: "'Playfair-Display', serif",
+            paddingLeft: "2.5rem",
+            paddingRight: "3.25rem",
+            paddingBottom: "6.19rem",
+            fontWeight: 400,
+            lineHeight: "normal",
+            fontSize: "3.82rem",
           }}
         >
-          <img
-            src="/Workmark white.png"
-            alt="When Women Understand Logo"
-            style={{
-              height: "auto",
-              width: "auto",
-              maxWidth: "12rem",
-              maxHeight: "auto",
-              paddingLeft: "2.5rem",
-              paddingTop: "2rem",
-            }}
-          ></img>
-          <div
-            className="align-self-end justify-self-start text-start text-light"
-            style={{
-              fontFamily: "'Playfair-Display', serif",
-              paddingLeft: "2.5rem",
-              paddingRight: "3.25rem",
-              paddingBottom: "6.5rem",
-              fontWeight: 400,
-              lineHeight: "normal",
-              fontSize: "5rem",
-            }}
-          >
-            Together, let's empower, support, and heal
-          </div>
+          Together, let's empower, support, and heal
         </div>
-        <div className="form-container col d-flex justify-content-center align-items-center">
-          <PassageAuth />
-        </div>
+      </div>
+      <div className="form-container col d-flex justify-content-center align-items-center">
+        <PassageAuth />
       </div>
     </div>
   );
